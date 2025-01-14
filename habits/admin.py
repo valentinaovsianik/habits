b0c4750970cfb0e6113b490000b223d3136e032e
Valentina Ovsianik <forovp@gmail.com>
2025-01-14 20:50:05 +0300

    Регистрация в админке

---
+++ habits/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Habit
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    list_display = ("id", "action", "owner", "time", "place", "pleasant", "linked_habit", "reward", "public", "duration", "periodicity")
+    list_filter = ("pleasant", "public", "periodicity")
+    search_fields = ("action", "owner__email", "owner__tg_nik")
