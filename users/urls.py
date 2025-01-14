8b002332206698b6ee15452fe66d2acf962404a6
Valentina Ovsianik <forovp@gmail.com>
2025-01-14 20:44:08 +0300

    Маршруты для Users

---
+++ users/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from rest_framework.permissions import AllowAny
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from users.views import UserCreateAPIView, UserViewSet
+
+app_name = "users"
+
+
+router = DefaultRouter()
+router.register(r"user", UserViewSet, basename="user")
+
+urlpatterns = [
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh",),
+] + router.urls
